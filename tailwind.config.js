2622012892384a60bc6180235d61fa57c8791297
kobanago <kobanago3800@gmail.com>
2024-03-20 17:30:37 +0900

    feat：tailwind：icon：設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,16 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: ['class', '[data-mode="dark"]'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bounce-short': 'bounce 1s ease-in-out 5.5',
+      },
+    },
   },
   plugins: [
     iconsPlugin({
       // https://icones.js.org/
-      collections: getIconCollections(['material-symbols', 'streamline']),
+      collections: getIconCollections(['material-symbols', 'streamline', 'emojione', 'ph']),
     }),
   ],
 };
